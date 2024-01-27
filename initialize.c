acdc2858d7674c6267e58e342428b68c46bdb898
Oussama-laaroubi <oussamalaarroubi@gmail.com>
2024-01-27 11:16:14 +0100

    used bool type insted of int

--- initialize.c
+++ initialize.c
@@ -6,7 +6,7 @@
 /*   By: olaaroub <olaaroub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 15:39:26 by olaaroub          #+#    #+#             */
-/*   Updated: 2024/01/27 11:10:56 by olaaroub         ###   ########.fr       */
+/*   Updated: 2024/01/27 11:15:43 by olaaroub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,26 +29,26 @@ void	fill_stack(char **av, t_node **head, t_node **tail)
 {
 	char	**ptr;
 	int		k;
-	int		first_time;
+	bool	first_time;
 	int		index;
 
 	ptr = NULL;
 	k = 1;
-	first_time = 1;
+	first_time = true;
 	while (av[k])
 	{
 		index = 0;
 		ptr = ft_split(av[k], ' ');
-		if (first_time == 1)
+		if (first_time == true)
 		{
 			start_stack(head, tail, ptr);
 		}
-		while (ptr[index] && first_time == 2)
+		while (ptr[index] && first_time == false)
 		{
 			append_beggining(head, atoi(ptr[index]));
 			index++;
 		}
-		first_time = 2;
+		first_time = false;
 		k++;
 	}
 }
